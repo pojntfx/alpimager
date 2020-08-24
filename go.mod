a7774ea602020b1de98fbfaea5649c872ba3ed17
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-08-24 21:30:53 +0200

    feat: Copy setup files into container

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/pojntfx/alpass
 go 1.14
 
 require (
-	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/Microsoft/hcsshim v0.8.9 // indirect
+	github.com/containerd/continuity v0.0.0-20200710164510-efbc4488d8fe // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v1.13.1
+	github.com/docker/docker v17.12.0-ce-rc1.0.20190725163905-fa8dd90ceb7b+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20200824131525-c12d262b63d8 // indirect
+	google.golang.org/grpc v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -9,13 +9,26 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+)
+
+const (
+	SETUP_SCRIPT_FILE_NAME    = "setup.sh"
+	PACKAGE_LIST_FILE_NAME    = "packages.txt"
+	REPOSITORY_LIST_FILE_NAME = "repositories.txt"
+	OUTPUT_IMAGE_FILE_NAME    = "alpine.qcow2"
+
+	DOCKER_IMAGE_URL = "docker.io/library/alpine:edge"
+	DOCKER_IMAGE     = "alpine:edge"
+
+	WORKDIR = "/tmp"
 )
 
 func main() {
-	script := flag.String("script", "setup.sh", "Configuration script file")
-	packages := flag.String("packages", "packages.txt", "Package list file")
-	repositories := flag.String("repositories", "repositories.txt", "Repostory list file")
-	output := flag.String("output", "alpine.qcow2", "Output file")
+	script := flag.String("script", SETUP_SCRIPT_FILE_NAME, "Setup script file")
+	packages := flag.String("packages", PACKAGE_LIST_FILE_NAME, "Package list file")
+	repositories := flag.String("repositories", REPOSITORY_LIST_FILE_NAME, "Repository list file")
+	output := flag.String("output", OUTPUT_IMAGE_FILE_NAME, "Output image file")
 
 	flag.Parse()
 
@@ -29,16 +42,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if _, err := cli.ImagePull(ctx, "docker.io/library/alpine:edge", types.ImagePullOptions{}); err != nil {
+	if _, err := cli.ImagePull(ctx, DOCKER_IMAGE_URL, types.ImagePullOptions{}); err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err := cli.ContainerCreate(ctx, &container.Config{Image: "alpine:edge", Cmd: []string{"tail", "-f", "/dev/null"}}, &container.HostConfig{Privileged: true}, nil, "")
+	resp, err := cli.ContainerCreate(ctx, &container.Config{Image: DOCKER_IMAGE, Cmd: []string{"tail", "-f", "/dev/null"}}, &container.HostConfig{Privileged: true}, nil, "")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		log.Fatal(err)
+	}
+
+	filePaths := []string{*script, *packages, *repositories}
+	for _, filePath := range filePaths {
+		archive, err := archive.Tar(filePath, archive.Gzip)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cli.CopyToContainer(ctx, resp.ID, WORKDIR, archive, types.CopyToContainerOptions{}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
 		log.Fatal(err)
 	}
 
